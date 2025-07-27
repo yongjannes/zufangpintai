d27fdda50596062e6052f19d838a70f5e59730fd
yongjannes <2380444947@qq.com>
2025-07-27 17:19:58 +0800

    房间支付方式管理接口实现

---
+++ common/src/main/java/com/zufang/lease/common/mybatisplus/MybatisMetaObjectHandler.java
@@ -0,0 +1,27 @@
+package com.zufang.lease.common.mybatisplus;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @Title: MybatisMetaObjectHandler
+ * @Author 殇枫
+ * @Package com.zufang.lease.common.mybatisplus
+ * @Date 2025/7/27
+ * @description:
+ */
+@Component
+public class MybatisMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
+    }
+}
--- web/web-admin/src/main/java/com/zufang/lease/web/admin/controller/apartment/PaymentTypeController.java
+++ web/web-admin/src/main/java/com/zufang/lease/web/admin/controller/apartment/PaymentTypeController.java
@@ -3,8 +3,10 @@ package com.zufang.lease.web.admin.controller.apartment;
 
 import com.zufang.lease.common.result.Result;
 import com.zufang.lease.model.entity.PaymentType;
+import com.zufang.lease.web.admin.service.PaymentTypeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,37 +17,30 @@ import java.util.List;
 @RestController
 public class PaymentTypeController {
 
+    @Autowired
+    private PaymentTypeService service;
+
     @Operation(summary = "查询全部支付方式列表")
     @GetMapping("list")
     public Result<List<PaymentType>> listPaymentType() {
-        return Result.ok();
+        List<PaymentType> list = service.list();
+        return Result.ok(list);
     }
 
     @Operation(summary = "保存或更新支付方式")
     @PostMapping("saveOrUpdate")
-    public Result saveOrUpdatePaymentType(@RequestBody PaymentType paymentType) {
+    public Result saveOrUpdatePaymentType(@RequestBody PaymentType paymentType)
+    {
+        service.saveOrUpdate(paymentType);
         return Result.ok();
     }
 
     @Operation(summary = "根据ID删除支付方式")
     @DeleteMapping("deleteById")
     public Result deletePaymentById(@RequestParam Long id) {
+        service.removeById(id);
         return Result.ok();
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
