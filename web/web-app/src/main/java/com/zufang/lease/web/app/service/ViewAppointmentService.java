587e82e3867f3735796006ac22f764a5ff14a080
yongjannes <2380444947@qq.com>
2025-07-28 21:52:26 +0800

    看房预约信息实现

--- web/web-app/src/main/java/com/zufang/lease/web/app/service/ViewAppointmentService.java
+++ web/web-app/src/main/java/com/zufang/lease/web/app/service/ViewAppointmentService.java
@@ -2,6 +2,10 @@ package com.zufang.lease.web.app.service;
 
 import com.zufang.lease.model.entity.ViewAppointment;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.zufang.lease.web.app.vo.appointment.AppointmentDetailVo;
+import com.zufang.lease.web.app.vo.appointment.AppointmentItemVo;
+
+import java.util.List;
 
 /**
 * @author liubo
@@ -9,4 +13,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @createDate 2023-07-26 11:12:39
 */
 public interface ViewAppointmentService extends IService<ViewAppointment> {
+    List<AppointmentItemVo> listItemByUserId(Long userId);
+
+    AppointmentDetailVo getDetailById(Long id);
 }
--- web/web-app/src/main/java/com/zufang/lease/web/app/service/impl/ViewAppointmentServiceImpl.java
+++ web/web-app/src/main/java/com/zufang/lease/web/app/service/impl/ViewAppointmentServiceImpl.java
@@ -2,9 +2,17 @@ package com.zufang.lease.web.app.service.impl;
 
 import com.zufang.lease.model.entity.ViewAppointment;
 import com.zufang.lease.web.app.mapper.ViewAppointmentMapper;
+import com.zufang.lease.web.app.service.ApartmentInfoService;
 import com.zufang.lease.web.app.service.ViewAppointmentService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zufang.lease.web.app.vo.apartment.ApartmentItemVo;
+import com.zufang.lease.web.app.vo.appointment.AppointmentDetailVo;
+import com.zufang.lease.web.app.vo.appointment.AppointmentItemVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author liubo
@@ -15,6 +23,30 @@ import org.springframework.stereotype.Service;
 public class ViewAppointmentServiceImpl extends ServiceImpl<ViewAppointmentMapper, ViewAppointment>
         implements ViewAppointmentService {
 
+    @Autowired
+    private ViewAppointmentMapper viewAppointmentMapper;
+
+    @Autowired
+    private ApartmentInfoService apartmentInfoService;
+
+    @Override
+    public List<AppointmentItemVo> listItemByUserId(Long userId) {
+        return viewAppointmentMapper.listItemByUserId(userId);
+    }
+
+    @Override
+    public AppointmentDetailVo getDetailById(Long id) {
+        ViewAppointment viewAppointment = viewAppointmentMapper.selectById(id);
+
+        ApartmentItemVo apartmentItemVo = apartmentInfoService.selectApartmentItemVoById(viewAppointment.getApartmentId());
+
+        AppointmentDetailVo agreementDetailVo = new AppointmentDetailVo();
+        BeanUtils.copyProperties(viewAppointment, agreementDetailVo);
+
+        agreementDetailVo.setApartmentItemVo(apartmentItemVo);
+
+        return agreementDetailVo;
+    }
 }
 
 
