9e8b705d7f78d1067ced69bf70d42526992b96e9
yongjannes <2380444947@qq.com>
2025-07-28 15:40:14 +0800

    后台用户信息管理接口实现、用户密码处理

--- web/web-admin/src/main/java/com/zufang/lease/web/admin/service/impl/SystemUserServiceImpl.java
+++ web/web-admin/src/main/java/com/zufang/lease/web/admin/service/impl/SystemUserServiceImpl.java
@@ -1,9 +1,16 @@
 package com.zufang.lease.web.admin.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.zufang.lease.model.entity.SystemPost;
 import com.zufang.lease.model.entity.SystemUser;
+import com.zufang.lease.web.admin.mapper.SystemPostMapper;
 import com.zufang.lease.web.admin.mapper.SystemUserMapper;
 import com.zufang.lease.web.admin.service.SystemUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zufang.lease.web.admin.vo.system.user.SystemUserItemVo;
+import com.zufang.lease.web.admin.vo.system.user.SystemUserQueryVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,6 +22,28 @@ import org.springframework.stereotype.Service;
 public class SystemUserServiceImpl extends ServiceImpl<SystemUserMapper, SystemUser>
         implements SystemUserService {
 
+    @Autowired
+    private SystemUserMapper systemUserMapper;
+
+    @Autowired
+    private SystemPostMapper systemPostMapper;
+
+    @Override
+    public IPage<SystemUserItemVo> pageSystemUserByQuery(IPage<SystemUser> page, SystemUserQueryVo queryVo) {
+        return systemUserMapper.pageSystemUserByQuery(page, queryVo);
+    }
+
+    @Override
+    public SystemUserItemVo getSystemUserById(Long id) {
+        SystemUser systemUser = systemUserMapper.selectById(id);
+
+        SystemPost systemPost = systemPostMapper.selectById(systemUser.getPostId());
+
+        SystemUserItemVo systemUserItemVo = new SystemUserItemVo();
+        BeanUtils.copyProperties(systemPost, systemUserItemVo);
+        systemUserItemVo.setPostName(systemUserItemVo.getPostName());
+        return systemUserItemVo;
+    }
 }
 
 
