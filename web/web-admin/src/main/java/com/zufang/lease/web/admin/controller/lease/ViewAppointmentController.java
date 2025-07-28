d70470eb59f362ec21f10edc7b0fd497c5154fcd
yongjannes <2380444947@qq.com>
2025-07-28 14:47:06 +0800

    预约看房管理接口实现

--- web/web-admin/src/main/java/com/zufang/lease/web/admin/controller/lease/ViewAppointmentController.java
+++ web/web-admin/src/main/java/com/zufang/lease/web/admin/controller/lease/ViewAppointmentController.java
@@ -1,13 +1,18 @@
 package com.zufang.lease.web.admin.controller.lease;
 
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zufang.lease.common.result.Result;
+import com.zufang.lease.model.entity.ViewAppointment;
 import com.zufang.lease.model.enums.AppointmentStatus;
+import com.zufang.lease.web.admin.service.ViewAppointmentService;
 import com.zufang.lease.web.admin.vo.appointment.AppointmentQueryVo;
 import com.zufang.lease.web.admin.vo.appointment.AppointmentVo;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -16,15 +21,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ViewAppointmentController {
 
+    @Autowired
+    private ViewAppointmentService service;
+
     @Operation(summary = "分页查询预约信息")
     @GetMapping("page")
     public Result<IPage<AppointmentVo>> page(@RequestParam long current, @RequestParam long size, AppointmentQueryVo queryVo) {
-        return Result.ok();
+        IPage<AppointmentVo> page = new Page<>(current, size);
+        IPage<AppointmentVo> list = service.pageAppointmentByQuery(page, queryVo);
+        return Result.ok(list);
     }
 
     @Operation(summary = "根据id更新预约状态")
     @PostMapping("updateStatusById")
-    public Result updateStatusById(@RequestParam Long id, @RequestParam AppointmentStatus status) {
+    public Result updateStatusById(@RequestParam Long id, @RequestParam Integer status) {
+        LambdaUpdateWrapper<ViewAppointment> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper.eq(ViewAppointment::getId, id);
+        updateWrapper.set(ViewAppointment::getAppointmentStatus, status);
+        service.update(updateWrapper);
         return Result.ok();
     }
 
--- web/web-admin/src/main/java/com/zufang/lease/web/admin/service/impl/ViewAppointmentServiceImpl.java
+++ web/web-admin/src/main/java/com/zufang/lease/web/admin/service/impl/ViewAppointmentServiceImpl.java
@@ -1,9 +1,13 @@
 package com.zufang.lease.web.admin.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.zufang.lease.model.entity.ViewAppointment;
 import com.zufang.lease.web.admin.mapper.ViewAppointmentMapper;
 import com.zufang.lease.web.admin.service.ViewAppointmentService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zufang.lease.web.admin.vo.appointment.AppointmentQueryVo;
+import com.zufang.lease.web.admin.vo.appointment.AppointmentVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,6 +19,13 @@ import org.springframework.stereotype.Service;
 public class ViewAppointmentServiceImpl extends ServiceImpl<ViewAppointmentMapper, ViewAppointment>
         implements ViewAppointmentService {
 
+    @Autowired
+    private ViewAppointmentMapper viewAppointmentMapper;
+
+    @Override
+    public IPage<AppointmentVo> pageAppointmentByQuery(IPage<AppointmentVo> page, AppointmentQueryVo queryVo) {
+        return viewAppointmentMapper.pageAppointmentByQuery(page, queryVo);
+    }
 }
 
 
